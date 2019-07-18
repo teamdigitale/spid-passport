e5eb5831d1abbf64ba11fef4c9fdff32fc10015f
Edigio <egidio.filosa@gmail.com>
2019-07-18 15:36:15 +0200

    [#167346522] Moved the Signature tag
    Moved the signature before SPSSODescriptor tag

--- index.js
+++ index.js
@@ -363,7 +363,9 @@ SpidStrategy.prototype.generateServiceProviderMetadata = function(
     "http://www.w3.org/2001/04/xmlenc#sha256"
   );
   sig.signatureAlgorithm = "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256";
-  sig.computeSignature(xml);
+  sig.computeSignature(xml,{
+    location: { reference: "//*[local-name(.)='SPSSODescriptor']", action: "before" } //This will place the signature before the SPSSODescriptor tag
+  });
 
   return sig.getSignedXml();
 };
